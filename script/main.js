08f7f08ee2418c7db723fac817d63c52ba03a071
coluccifrancesco <coluccif2002@gmail.com>
2025-06-16 19:50:57 +0200

    Update HTML structure and streamline fetch logic in main.js, rewrite Js follow documentation and not copy

--- script/main.js
+++ script/main.js
@@ -1,18 +1,18 @@
-const myHeaders = new Headers();
-myHeaders.append("Cookie", "XSRF-TOKEN=eyJpdiI6IktUOXpsNThXRVl5RXowejB1bFRlbXc9PSIsInZhbHVlIjoiVzFDaDRrNE9hbklBWVBENm1NbVdOYnRUd3VJMGpCMVNWNk1yajRYeXhcL0FJdmdmbHpsTDVmbEVDdmxGaWUrckMiLCJtYWMiOiI1MzZkZjA1ODMzZTNiOTRkOGJiOTFmNDhmNThhNDJlYWZjYWEyYjlhMDI2Y2ZmMWYwYmI0YTg2MWFjZTJkZTVmIn0%3D; laravel_session=eyJpdiI6IllQZnFQZ3NMdVZ5dDM4ZjhkbldjSFE9PSIsInZhbHVlIjoiWUl6YUdMcjkxdFJPSG9oXC9PVHhldUwyTGk4V3ArMDkyblpxMG9ERmpvc0g1ZmtUXC9ET21KUW81TkoxdXVxUFwvaSIsIm1hYyI6IjIxOWI2OTk0MjEyYWRmNjc2YTEwZWZiMDYyMjcyNDI5N2RiNDkzOGM0OGE0YzVjNjhhZDdkNDNlZTFkNDA4M2IifQ%3D%3D");
+for (let i = 0; i < 10; i++) {
+    fetch('https://flynn.boolean.careers/exercises/api/random/mail')  // facciamo la chiamata http
+    .then(response => {
 
-const requestOptions = {
-method: "GET",
-headers: myHeaders,
-redirect: "follow"
-};
+        // "lancio" un errore che viene poi "preso" in caso la risposta non sia 'ok'
+        if(!response.ok){
+            throw new Error ('Could not fetch the resource');
+        } 
+            
+        return response.json();
 
-for(let i = 0; i < 10; i++){
-   
-    fetch("https://flynn.boolean.careers/exercises/api/random/mail", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.error(error));
+    })  // prendiamo la risposta e la rendiamo leggibile in Json
+        
+    .then(data => console.log(data.response))    // stampo tra i dati, la risposta della richiesta
+    .catch(error => console.error(error));      // in caso di errore lo logghiamo
 
 }
 
