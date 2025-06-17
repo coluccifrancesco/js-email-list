2a47e921c1b5e25ca10a8804abc12843660e4f13
coluccifrancesco <coluccif2002@gmail.com>
2025-06-17 09:32:50 +0200

    Refactor email fetching and rendering logic in main.js; update HTML structure to remove unused elements

--- script/main.js
+++ script/main.js
@@ -1,5 +1,8 @@
+const emails = [];
+
 for (let i = 0; i < 10; i++) {
     fetch('https://flynn.boolean.careers/exercises/api/random/mail')  // facciamo la chiamata http
+    
     .then(response => {
 
         // "lancio" un errore che viene poi "preso" in caso la risposta non sia 'ok'
@@ -10,12 +13,37 @@ for (let i = 0; i < 10; i++) {
         return response.json();
 
     })  // prendiamo la risposta e la rendiamo leggibile in Json
-        
-    .then(data => console.log(data.response))    // stampo tra i dati, la risposta della richiesta
-    .catch(error => console.error(error));      // in caso di errore lo logghiamo
+     
 
+    .then(data => {
+        // stampo tra i dati, la risposta della richiesta
+        console.log(data.response);
+        
+        // inserisco le mail generate in un array
+        emails.push(data.response);
+    })
+    
+    .catch(error => console.error(error));      // in caso di errore lo logghiamo
 }
 
+console.log(emails);
+
+// Inserisco gli elementi nel DOM
+const listCont = document.getElementById('mail-container');
+
+const listEl = document.createElement('ul');
+
+for (let i = 0; i < emails.length; i++) {
+    let mail = emails[i];
+    console.log(mail);
+    
+    const listItem = document.createElement('li');
+    
+    listItem.textContent = mail;
+    listEl.appendChild(listItem);
+}
+
+listCont.appendChild(listEl)
 
 // Bonus (opzionale)
 // Abbellire con CSS o Bootstrap
